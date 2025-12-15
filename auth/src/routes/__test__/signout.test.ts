f3f5f340ec7dadade7e538d93b6f436404c549c7
amkh3780 <khandan.work80@gmail.com>
2025-12-15 15:03:48 +0330

    test: make signout cookie assertion tolerant

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -17,7 +17,8 @@ it("clears the cookies after signing out", async () => {
 
   //   console.log(response.get("Set-Cookie"));
 
-  expect(response.get("Set-Cookie")[0]).toEqual(
-    "session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly" // This value is from response.get("Set-Cookie")
-  );
+  const cookie = response.get("Set-Cookie")[0];
+  expect(cookie).toContain("session=;");
+  expect(cookie).toContain("expires=Thu, 01 Jan 1970 00:00:00 GMT");
+  expect(cookie.toLowerCase()).toContain("httponly");
 });
